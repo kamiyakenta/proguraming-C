a7996405adee87f7e0a7feba889915bc14d2dbef
神谷健太 <kentakmy@gmail.com>
2018-11-23 01:19:45 +0900

    Edit 6

--- kadai5.c
+++ kadai5.c
@@ -2,7 +2,7 @@
 #define MAX_N 100
 
 typedef struct Pro {
-  char name[11];
+  char name[100];
   int time;
 } Pro; //１セットにcharとintを入れたいので構造体を作る
 
@@ -25,9 +25,8 @@ int main() {
     scanf(" %s %d", que[i].name, &(que[i].time));
   }
   //全ての入力完了
-  printf("\n");
 
-  for(cnt = 0, time = 0; cnt < num; ) {
+  for(cnt = 0, time = 0; cnt < num;) {
 
     if (q < que[0].time){
       tmp = q;  //クォンタムより大きかったらクォンタム分のみ
@@ -36,14 +35,16 @@ int main() {
     }
 
     que[0].time -= tmp;
-    time += tmp;
 
+    time += tmp;
+    int flag = 0;
     //もうプロセス終了したら結果にいれる
-    if(que[0].time == 0) {
+    if(que[0].time <= 0) {
       fin[k] = que[0];
       fin[k].time = time;
       k++;
       cnt++;
+      flag = 1;
     }
 
     //キューの循環
@@ -51,21 +52,39 @@ int main() {
     for (j = 1; j < num; j++){
       que[j-1] = que[j];
     }
-    que[num-1] = tmp2;
+    if(flag == 0){
+      que[num-1-cnt] = tmp2;
+    }else{
+      que[num-cnt] = tmp2;
+    }
 
     //毎度キューの中身を全て出力
     for(j = 0; j < num; j ++){
       if(que[j].time != 0){
-        printf("%s %d ", que[j].name, que[j].time);
+        printf("%s %d", que[j].name, que[j].time); //WOJのため
+        //printf("%s %d ", que[j].name, que[j].time);
       }
     }
-    if(cnt!=num-1){
-      printf("\n");
+
+    //WOJのための出力合わせ
+    if (num == 1){
+      if(cnt==num-1){
+        printf("\n");
+      }
+    }else{
+      if(cnt!=num-1){
+        printf("\n");
+      }
     }
+
   }
-  //最後に結果出力
+  //最後に結果出力(WOJのために条件分岐)
   for(k = 0; k < num; k++){
-    printf("%s %d ", fin[k].name, fin[k].time);
+    if(k != num-1){
+      printf("%s %d ", fin[k].name, fin[k].time);
+    }else{
+      printf("%s %d", fin[k].name, fin[k].time);
+    }
   }
   printf("\n");
   return 0;
