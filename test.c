2468ceb3f3056c3a8ba56da9d0e8591a5115feba
神谷健太 <kentakmy@gmail.com>
2018-11-26 00:01:05 +0900

    Edit 9

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// int main(void)
+// {
+//   int k=0;
+//   (k++)++;
+//   printf("%d\n", k);
+//   return 0;
+// }
+
+int main(void){
+  int k =7;
+  printf("%d\n", k++ * k++);
+  return 0;
+}
