446ecbd893bba6844df504fc4e13d7ff308c446f
神谷健太 <kentakmy@gmail.com>
2018-12-01 20:39:48 +0900

    Edit 11

--- kadai12.c
+++ kadai12.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void mindif(int a[], int n){
+  int i, j;
+  int dif;
+  int min;
+  min = abs(a[0]-a[1]);
+  for(i = 0; i < n-1; i++){
+    for(j = i+1; j < n; j++){
+      dif = abs(a[i]-a[j]);
+      if( min > dif ){
+        min = dif;
+      }
+    }
+  }
+  printf("%d\n", min);
+}
+
 
 int main(void){
   int n;
   int i;
-  int a[n];
 
   while(1){
     scanf("%d", &n);
     if(n==0) break;
+    int a[n];
     for(i=0; i<n; i++){
       scanf("%d", &a[i]);
     }
-    printf("Hello\n");
+    mindif(a, n);
   }
-
   return 0;
 }
