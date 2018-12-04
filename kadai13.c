3ddb1172ce873c62c237fe93998733173305adb5
神谷健太 <kentakmy@gmail.com>
2018-12-04 17:59:00 +0900

    Edit 17

--- kadai13.c
+++ kadai13.c
@@ -7,35 +7,26 @@
 // 入力の終わりは0を二つ含む行で表される．
 
 // 出力: 入力のデータセットごとに， 最初の位置から到達可能な区画数を1行で出力して改行しなさい(最初の区画も含む)．
+
 #include <stdio.h>
 
-#define N 21
+#define N 21 //外枠も考える
 
-char A[N][N];
-int check[N][N];
+char Map[N][N];
+int flag[N][N];
 
-void search(int s, int t) {
-  // check whether s, t in array range
-  if (s < 0 || s >= N || t < 0 || t >= N) {
-    return;
-  } else if (A[s][t] == '#') {
-    // when A[s][t] = '#'
-    return;
-  } else if (check[s][t] == 1) {
-    // when already checked
-    return;
-  } else {
-    // when s, t in array range
-    if (A[s][t] == '.' || A[s][t] == '@') {
-      check[s][t] = 1;
-      // check top
-      search(s-1, t);
-      // check right
-      search(s, t+1);
-      // check left
-      search(s, t-1);
-      // check bottom
-      search(s+1, t);
+void search(int x, int y) {
+  if (x < 0 || x >= N || y < 0 || y >= N) return;
+  else if(Map[x][y] == '#') return;
+  else if(flag[x][y] == 1) return;
+  else {
+    if (Map[x][y] == '.' || Map[x][y] == '@') {
+      flag[x][y] = 1;
+      //四方向で探す
+      search(x-1, y);
+      search(x, y+1);
+      search(x, y-1);
+      search(x+1, y);
     }
   }
 }
@@ -44,37 +35,37 @@ int main(void) {
 
   int W, H;
   int i, j;
-  int s, t;
+  int x, y; //現在地
   int count;
 
   while (1) {
     scanf("%d", &W);
     scanf("%d", &H);
-
-    if (W == 0 && H == 0) break;
-
     count = 0;
 
-    // initialize A '0'
+    if (W == 0 || H == 0) break;
+
+    //地図の入力
     for (i = 0; i < H; i++) {
-      scanf("%s", A[i]);
+      scanf("%s", Map[i]);
     }
+
     for (i = 0; i < N; i++) {
       for (j = 0; j < N; j++) {
-        check[i][j] = 0;
-        if (i >= H || j >= W) A[i][j] = '0';
-        if (A[i][j] == '@') {
-          s = i;
-          t = j;
+        flag[i][j] = 0;  //通ってないところは０
+        if (i >= H || j >= W) Map[i][j] = '0';
+        if (Map[i][j] == '@') { //スタート地点
+          x = i;
+          y = j;
         }
       }
     }
-    search(s, t);
+    search(x, y);
 
     // output
     for (i = 0; i < H; i++) {
       for (j = 0; j < W; j++) {
-        if (check[i][j] == 1) count++;
+        if (flag[i][j] == 1) count++;
       }
     }
     printf("%d\n", count);
