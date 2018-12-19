f8a12f47b702767ae754740b58018f639e65c240
神谷健太 <kentakmy@gmail.com>
2018-12-19 20:32:50 +0900

    Edit 24

--- kadai33.c
+++ kadai33.c
@@ -1,5 +1,28 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_8_C#
+/*  sample input
+18
+insert 8
+insert 2
+insert 3
+insert 7
+insert 22
+insert 1
+find 1
+find 2
+find 3
+find 4
+find 5
+find 6
+find 7
+find 8
+print
+delete 3
+delete 7
+print
+*/
+
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct{
@@ -8,7 +31,7 @@ typedef struct{
     int left;
     int right;
 }Node;
-
+ 
 Node A[500000];
 
 void inParse(int p){
@@ -22,35 +45,32 @@ void preParse(int p){
     if(A[p].left>=0) preParse(A[p].left);
     if(A[p].right>=0) preParse(A[p].right);
 }
+
+void insert(int z){
+    int x = 0;
+    int y = -1;
     
-void insert(int count, int z){
-    int x=0, y;
-    if(count==0) x=-1;
+    if(z != 0)
+        while(x != -1){
+            y = x;
+            if(A[z].value < A[x].value) x = A[x].left;
+            else x = A[x].right;
+        }
     
-    A[count].value = z;
-    A[count].left = -1;
-    A[count].right = -1;
+    A[z].parent = y;
     
-    while(x>=0){
-        y = x; 
-        if(z < A[x].value)x = A[x].left; 
-        else x = A[x].right; 
+    if(y != -1){
+        if(A[z].value < A[y].value) A[y].left = z;
+        else A[y].right = z;
     }
-    if(count<1){}
-    else if(z < A[y].value)A[y].left = count; 
-    else A[y].right = count; 
+    return ;
 }
 
-int search(int node, int num){
-    int t; 
-    t = num - A[node].value;
-    if (t==0) return 1;
-    if (t < 0) {
-        if (A[node].left > 0) return search(A[node].left, num);
-    } else if (t > 0) {
-        if (A[node].right > 0) return search(A[node].right, num);
-    }
-    return 0;
+void search(int x, int k){
+    if(x == -1) printf("no\n");
+    else if(A[x].value == k) printf("yes\n");
+    else if(k < A[x].value) search(A[x].left, k);
+    else if(k > A[x].value) search(A[x].right, k);
 }
 
 int successor(int x){
@@ -68,68 +88,58 @@ int successor(int x){
         return y;
     }
 }
-    
+ 
 void delete(int k){
     int x, y, z = 0;
-        
-    while(1){
+
+    while(1){ //探す
         if(A[z].value == k) break;
         else if(k < A[z].value) z = A[z].left;
         else z = A[z].right;
     }
-        
+
     if(A[z].left == -1 || A[z].right == -1) y = z;
     else y = successor(z);
-        
+
     if(A[y].left != -1) x = A[y].left;
     else x = A[y].right;
-        
+
     if(x != -1) A[x].parent = A[y].parent;
-        
+
     if(A[y].parent != -1){
         if(y == A[A[y].parent].left) A[A[y].parent].left = x;
         else A[A[y].parent].right = x;
     }
     if(y !=  z) A[z].value = A[y].value;
-    
+    return ;
 }
 
 int main(){
-    char s[8];
-    int n;
+    int i, n;
     int num;
-    int count=0;
-    int i;
-    int flag = 0;
-    scanf("%d", &n);  
-    for(i=0; i<n; i++){
-        scanf("%s", s);
-        if(strcmp(s, "insert") == 0){ //insert
-            scanf("%d", &num); 
-            insert(count, num); 
-            count++; 
-        }
-        if(strcmp(s, "find") == 0){ //find
-            scanf("%d", &num);
-            flag = search(0, num);
-            if (flag) {
-                printf("yes\n");
-            } else {
-                printf("no\n");
-            }
-        }
+    char s[7];
+    scanf("%d", &n);
 
-        if(strcmp(s, "delete") == 0){ //delete
+    for(i=0; i<n; i++){
+        A[i].left = -1;
+        A[i].right = -1;
+        scanf("%s", s);
+    
+        if(strcmp(s, "insert") == 0){
+            scanf("%d", &A[i].value);
+            insert(i);
+        }else if(strcmp(s, "print") == 0){
+            inParse(0);
+            printf("\n");
+            preParse(0);
+            printf("\n");
+        }else if(strcmp(s, "find") == 0){
+            scanf("%d", &num);
+            search(0, num);
+        }else if(strcmp(s, "delete") == 0){
             scanf("%d", &num);
             delete(num);
         }
-
-        if(strcmp(s, "print") == 0){ //print
-            if(count>0) inParse(0);
-            printf("\n");
-            if(count>0) preParse(0);
-            printf("\n");
-        }
-    }  
-    return 0;  
+    }
+    return 0;
 }
